10165cfd2af75a6043e6a09b3b32391bffd99b88
sandun2593 <132238367+sandun2593@users.noreply.github.com>
2023-05-19 09:32:24 +1200

    Add files via upload

--- Temperature Connverter.py
+++ Temperature Connverter.py
@@ -2,6 +2,10 @@ from tkinter import *
 
 class converter:
     def __init__(self):
+
+        button_font = ("Arial", "8")
+        button_fg = "#FFFFFF"
+
         self.temp_frame = Frame()
         self.temp_frame.grid()
 
@@ -19,6 +23,65 @@ class converter:
                                        wraplength=200, width=40,
                                        justify="left")
         self.temp_instructions.grid(row=1)
+
+        self.temp_entry = Entry(self.temp_frame,
+                                font=("Ariel", "14"))
+        self.temp_entry.grid(row=2, padx=10, pady=10)
+
+        error = "please enter a number"
+        self.temp_error = Label(self.temp_frame, text=error,
+                                fg="#9c0000")
+        self.temp_error.grid(row=3)
+
+        self.button_frame = Frame(self.temp_frame)
+        self.button_frame.grid(row=4)
+
+        self.to_celsius_button = Button(self.button_frame,
+                                        text="To Celsius",
+                                        bg="#990099",
+                                        fg="#FFFFFF",
+                                        command=self.to_celcius)
+        self.to_celsius_button.grid(row=0, column=0, padx=5, pady=5)
+
+        self.to_farenheit_button = Button(self.button_frame,
+                                          text="To Farenheit",
+                                          bg="#009900",
+                                          fg="#FFFFFF")
+        self.to_farenheit_button.grid(row=0, column=1, padx=5, pady=5)
+#
+        self.to_help_button = Button(self.button_frame,
+                                     text="Help / Info",
+                                     bg="#CC6600",
+                                     fg=button_fg,
+                                     font=button_font, width=12)
+        self.to_help_button.grid(row=1, column=0, padx=5, pady=5)
+
+        self.to_history_button = Button(self.button_frame,
+                                       text="History / Export",
+                                       bg="#004C99",
+                                       fg=button_fg,
+                                       font=button_font, width=12,
+                                       state=DISABLED)
+        self.to_history_button.grid(row=1, column=1, padx=5, pady=5)
+
+    def check_temp(min_value):
+        error = "pPlease enter a number that is more than {}".format(min_value)
+
+        try:
+            response = self.temp_entry.get()
+            response = float(response)
+
+            if response < min_value:
+                print(error)
+            else:
+                return response
+        except ValueError:
+            self.temp_error.config(text=error)
+
+    def to_celcius(self):
+        self.check_temp(-459)
+
+
 
 if __name__ == "__main__":
     root = Tk()
